d0f6aec216323523762c6ab9ca49d082400ff3d3
Gopal Goyal <51030583+gopal-goyal@users.noreply.github.com>
2022-05-26 19:20:31 +0530

    Added middleware

--- crudApi/settings.py
+++ crudApi/settings.py
@@ -53,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'crudApi.urls'
