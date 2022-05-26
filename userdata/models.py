d1dee4c4c49d7a00cf1677971f5ce1c0ea728107
Gopal Goyal <51030583+gopal-goyal@users.noreply.github.com>
2022-05-26 19:39:34 +0530

    Add models

--- userdata/models.py
+++ userdata/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Userdata(models.Model):
+    name = models.CharField(max_length=70, blank=False, default='')
+    username = models.CharField(max_length=70,blank=False, default='')
+    age = models.IntegerField(default=0)
+    dob = models.DateTimeField(auto_now=True)
+
